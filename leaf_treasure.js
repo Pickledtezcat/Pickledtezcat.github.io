a67f8948ad862e20fb7e05db8f3d324f61ed7bc6
Pickledtezcat <johnmtopple@hotmail.co.uk>
2018-11-28 02:00:19 +0900

    added open streetmaps
    added open streetmaps

--- leaf_treasure.js
+++ leaf_treasure.js
@@ -1,3 +1,9 @@
 
 var default_position = [35.855460054052756, 128.64783699078816]
-var mymap = L.map('mapid').setView(default_position, 13);
+var mymap = L.map('mapid').setView(default_position, 27);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+var marker = L.marker(default_position).addTo(mymap);
