f13516af54bbad1ebb6484c29cf98711254df132
Pickledtezcat <johnmtopple@hotmail.co.uk>
2018-11-28 01:42:29 +0900

    adding new maps method
    switching to open strret maps

---
+++ leaf_treasure.js
@@ -0,0 +1,3 @@
+
+var default_position = [35.855460054052756, 128.64783699078816]
+var mymap = L.map('mapid').setView(default_position, 13);
--- main_script.js
+++ main_script.js
@@ -2,14 +2,14 @@ var main_body = document.getElementById("body");
 
 var sub_button = document.createElement("button");
 main_body.appendChild(sub_button)
-sub_button.innerHTML = "Treasure Hunt!."
+sub_button.innerHTML = "Treasure Hunt! v2.0."
 
 sub_button.addEventListener('click', go_to_sub_page, false);
 
 
 function go_to_sub_page() {
-	window.location.assign("treasure_hunt.html");
-	
+	window.location.assign("leaf_treasure.html");
+
 }
 
 var bell_button = document.createElement("button");
@@ -21,5 +21,5 @@ bell_button.addEventListener('click', go_to_bell_jar, false);
 
 function go_to_bell_jar() {
 	window.location.assign("bell_jar.html");
-	
+
 }
