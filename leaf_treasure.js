4132cf9a73bb7e761e4c59c63ec3db93bf7ae1ec
Pickledtezcat <johnmtopple@hotmail.co.uk>
2018-11-28 21:13:43 +0900

    Update leaf_treasure.js
    testing data store

--- leaf_treasure.js
+++ leaf_treasure.js
@@ -1,4 +1,4 @@
-
+console.log("version 2.2")
 var default_position = [35.855460054052756, 128.64783699078816]
 var mymap = L.map('mapid').setView(default_position, 27);
 
@@ -12,7 +12,8 @@ var game_key = "testing";
 var emoji = document.getElementById('icon_to_use').value;
 var game_data = {test_data: "Hello world", emoji:emoji};
 
-GJAPI.DataStoreSet (GJAPI.DATA_STORE_GLOBAL, game_key, game_data, check_success)
+// GJAPI.DataStoreSet (GJAPI.DATA_STORE_GLOBAL, game_key, game_data, check_success);
+var data_keys = GJAPI.DataStoreGetKeys (GJAPI.DATA_STORE_GLOBAL, check_success);
 
 function check_success() {
   console.log("success")
