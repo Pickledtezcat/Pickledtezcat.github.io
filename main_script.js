ac0434edbbec2e8aae0ac31c815793572807ad75
Pickledtezcat <johnmtopple@hotmail.co.uk>
2018-11-29 01:48:33 +0900

    Update main_script.js
    update to 2.8

--- main_script.js
+++ main_script.js
@@ -3,7 +3,7 @@ var main_body = document.getElementById("body");
 var sub_button = document.createElement("button");
 sub_button.className = "button";
 main_body.appendChild(sub_button);
-sub_button.innerHTML = "Treasure Hunt! v2.7.";
+sub_button.innerHTML = "Treasure Hunt! v2.8.";
 
 sub_button.addEventListener('click', go_to_sub_page, false);
 
