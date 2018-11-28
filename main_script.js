c2d9661b6872e58db5f0cdf54da0b364d4d496ed
Pickledtezcat <johnmtopple@hotmail.co.uk>
2018-11-28 21:54:38 +0900

    Update main_script.js
    double dot

--- main_script.js
+++ main_script.js
@@ -1,9 +1,9 @@
 var main_body = document.getElementById("body");
 
 var sub_button = document.createElement("button");
-sub_button..setAttribute("className", "button");
-main_body.appendChild(sub_button)
-sub_button.innerHTML = "Treasure Hunt! v2.3."
+sub_button.setAttribute("className", "button");
+main_body.appendChild(sub_button);
+sub_button.innerHTML = "Treasure Hunt! v2.3.";
 
 sub_button.addEventListener('click', go_to_sub_page, false);
 
@@ -14,8 +14,8 @@ function go_to_sub_page() {
 }
 
 var bell_button = document.createElement("button");
-main_body.appendChild(bell_button)
-bell_button.innerHTML = "Play The Bell Jar."
+main_body.appendChild(bell_button);
+bell_button.innerHTML = "Play The Bell Jar.";
 
 bell_button.addEventListener('click', go_to_bell_jar, false);
 
