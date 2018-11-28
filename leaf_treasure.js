7acec09a45aa84bf8fd00936a9e82003f6358187
Pickledtezcat <johnmtopple@hotmail.co.uk>
2018-11-28 21:18:26 +0900

    Update leaf_treasure.js
    testing retreive key

--- leaf_treasure.js
+++ leaf_treasure.js
@@ -1,4 +1,4 @@
-console.log("version 2.2")
+console.log("version 2.3")
 var default_position = [35.855460054052756, 128.64783699078816]
 var mymap = L.map('mapid').setView(default_position, 27);
 
@@ -13,7 +13,8 @@ var emoji = document.getElementById('icon_to_use').value;
 var game_data = {test_data: "Hello world", emoji:emoji};
 
 // GJAPI.DataStoreSet (GJAPI.DATA_STORE_GLOBAL, game_key, game_data, check_success);
-var data_keys = GJAPI.DataStoreGetKeys (GJAPI.DATA_STORE_GLOBAL, check_success);
+// var data_keys = GJAPI.DataStoreGetKeys (GJAPI.DATA_STORE_GLOBAL, check_success);
+var game_data = GJAPI.DataStoreFetch (GJAPI.DATA_STORE_GLOBAL, game_key, check_success);
 
 function check_success() {
   console.log("success")
