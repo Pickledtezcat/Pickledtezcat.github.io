9777653f3c93900323fef6c9176af7e8f482d9d7
Pickledtezcat <johnmtopple@hotmail.co.uk>
2018-11-28 21:22:15 +0900

    stlying buttons
    styling buttons

--- main_script.js
+++ main_script.js
@@ -2,7 +2,7 @@ var main_body = document.getElementById("body");
 
 var sub_button = document.createElement("button");
 main_body.appendChild(sub_button)
-sub_button.innerHTML = "Treasure Hunt! v2.0."
+sub_button.innerHTML = "Treasure Hunt! v2.3."
 
 sub_button.addEventListener('click', go_to_sub_page, false);
 
