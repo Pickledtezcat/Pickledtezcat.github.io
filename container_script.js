2c8c0a6f11b4b2cf6e9c9c0d3260a72aa117d016
Pickledtezcat <johnmtopple@hotmail.co.uk>
2019-03-03 22:42:22 +0900

    Revert "reverting to flipcard"
    This reverts commit 2b93d2d303a1ba92619e074e9d4e3ba0204bd205.

--- container_script.js
+++ container_script.js
@@ -11,45 +11,36 @@ var sentences = document.getElementById("sentences");
 var pictures = document.getElementById("pictures");
 var flying_pictures = document.getElementById("flying_pictures");
 var home = document.getElementById("home");
+var flip_cards = document.getElementById("flip_cards");
 
 sentences.addEventListener('click', go_to_sentences, false);
 console.log(sentences.classList)
 
 function go_to_sentences() {
-  sentences.className = "active"
-  pictures.className = "inactive"
-  home.className = "inactive"
-  flying_pictures.className = "inactive"
   var selected = selector.value
 	window.location.assign("sentences.html?"+selected);
 }
 
 pictures.addEventListener('click', go_to_pictures, false);
 function go_to_pictures() {
-  sentences.className = "inactive"
-  pictures.className = "active"
-  home.className = "inactive"
-  flying_pictures.className = "inactive"
   var selected = selector.value
 	window.location.assign("pictures.html?"+selected);
 }
 
 home.addEventListener('click', go_to_home, false);
 function go_to_home() {
-  sentences.className = "inactive"
-  pictures.className = "inactive"
-  home.className = "active"
-  flying_pictures.className = "inactive"
   var selected = selector.value
 	window.location.assign("index.html");
 }
 
 flying_pictures.addEventListener('click', go_to_flying_pictures, false);
 function go_to_flying_pictures() {
-  sentences.className = "inactive"
-  pictures.className = "inactive"
-  home.className = "inactive"
-  flying_pictures.className = "active"
   var selected = selector.value
 	window.location.assign("flying_pictures.html?"+selected);
+}
+
+flip_cards.addEventListener('click', go_to_flip_cards, false);
+function go_to_flip_cards() {
+  var selected = selector.value
+	window.location.assign("flip_cards.html?"+selected);
 }
